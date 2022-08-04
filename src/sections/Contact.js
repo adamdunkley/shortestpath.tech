07329368e9a0c1756b2c1afac4d7e0707ad49977
Adam Dunkley <adamdunkley@users.noreply.github.com>
2022-08-04 10:47:44 +0100

    Update Contact.js

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -10,22 +10,25 @@ const ContactSection = styled(Section)`
   min-height: auto;
 `;
 
+// Removed calendly
+//
+//         <Box width={[1, 2/3, 1/2]} pl={3} pr={3} sx={{
+//             maxWidth: '512px'
+//           }}>
+//           <Heading as="h2" variant="subheading" textAlign={['left', 'left', 'left']}>
+//             Book an intro call
+//           </Heading>
+//           <Text variant="main" textAlign={['left', 'left', 'left']} mb={3}>
+//             Find a time below for a 30 minute call, I'd love to hear
+//             about your business.
+//           </Text>
+//           <Calendly />
+//         </Box>
+
 const Contact = () => (
   <Container>
     <ContactSection>
       <Flex flexWrap='wrap' justifyContent="center" pt={[4, 5, 5]}>
-        <Box width={[1, 2/3, 1/2]} pl={3} pr={3} sx={{
-            maxWidth: '512px'
-          }}>
-          <Heading as="h2" variant="subheading" textAlign={['left', 'left', 'left']}>
-            Book an intro call
-          </Heading>
-          <Text variant="main" textAlign={['left', 'left', 'left']} mb={3}>
-            Find a time below for a 30 minute call, I'd love to hear
-            about your business.
-          </Text>
-          <Calendly />
-        </Box>
         <Box width={[1, 2/3, 1/2]} mt={[4, 4, 0]}  pl={3} pr={3} sx={{
             maxWidth: '512px'
           }}>
